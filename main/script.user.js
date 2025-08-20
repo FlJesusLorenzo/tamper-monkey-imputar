fbb9bc7218cb5b48519b1dbba6d6feca00febe74
FlJesusLorenzo <jesus.lorenzo@factorlibre.com>
2025-08-20 17:00:28 +0200

    Update version

--- main/script.user.js
+++ main/script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Imputaciones con OdooRPC - Popup
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      2025-08-20
 // @description  Create timesheet entries directly from GitLab using OdooRPC popup
 // @author       Jesús Lorenzo
 // @match        https://git.factorlibre.com/*/issues/*
